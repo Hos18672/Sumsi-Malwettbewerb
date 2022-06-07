372d8c67a0ad58cc19c5016ca73f8f23f705116c
Patricio Schranz <patricio.schranz@edu.fh-kaernten.ac.at>
2022-06-07 08:49:54 +0200

    change language function

---
+++ src/js/change-language.js
@@ -0,0 +1,43 @@
+const languageElement = document.querySelectorAll('.change-language')
+const germanLanguage = document.querySelectorAll('.german')
+const englishLanguage = document.querySelectorAll('.english')
+
+let isGermanTrue = true
+
+
+
+// languageElement.addEventListener("click", changeTheLanguage())
+
+languageElement.forEach( (item) => {
+    item.addEventListener('click', changeTheLanguage)
+})
+
+
+function changeTheLanguage() {
+    console.log('hello')
+
+    if(isGermanTrue){
+        germanLanguage.forEach ( (item) => {
+            item.style.display = 'none'
+        })
+    
+        englishLanguage.forEach ( (item) => {
+            item.style.display = 'block'
+        })
+
+        isGermanTrue = false
+    }
+    else {
+        germanLanguage.forEach ( (item) => {
+            item.style.display = 'block'
+        })
+    
+        englishLanguage.forEach ( (item) => {
+            item.style.display = 'none'
+        })
+
+        isGermanTrue = true
+    }
+
+  
+}
