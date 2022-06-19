31389fecb40b88a4c58d40ca097be1cc6a3e8564
Patricio Schranz <patricio.schranz@edu.fh-kaernten.ac.at>
2022-06-19 20:08:05 +0200

    another bugs are fixed

--- src/js/close-the-hamburger.js
+++ src/js/close-the-hamburger.js
@@ -1,12 +1,27 @@
 const theHeaderLinks = document.querySelectorAll('.header li')
-const theNavi = document.getElementById('header_nav')
+const theNavi = document.querySelector('.header_nav')
+const theCheckbox = document.getElementById('hamburger-check')
+
+// let xy = theNavi.style
+// console.log(xy)
+// console.log(theNavi)
+
 
 console.log(theHeaderLinks)
 
 theHeaderLinks.forEach( link => {
-    link.addEventListener('click', () =>{
-       if(theNavi.style.width == '50vw'){
-        alert('the widht is changed')
-       }
+    link.addEventListener('click', (item) =>{
+    //     let xy = theNavi.style.position
+
+    //     if(theNavi.style.position == 'fixed'){
+    //         alert('hamburger is active')
+    //     }
+    //    alert(xy)
+
+    // theNavi.classList.add("get-out-of-view")
+
+    theCheckbox.checked = false
+    document.removeEventListener( 'click', this.Function)
+
     })
 })
